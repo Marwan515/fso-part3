f1db6867b507f6dd96f2afa1afea4e916c93ecb3
Marwan <Marwanabdulmannan@gmail.com>
2023-05-18 02:35:18 +0400

    exercise 3.7 and 3.8 done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const { json } = require("express/lib/response")
-
+const morgan = require("morgan")
 let phoneBook = [
   { 
     "id": 1,
@@ -26,12 +26,59 @@ let phoneBook = [
 
 const app = express()
 
+morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 app.get('/api/persons', (request, response) => {
   response.json(phoneBook)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const contact = phoneBook.find(c => c.id === id)
+  if (contact) {
+    response.json(contact)
+  } else {
+    response.status(404).end()
+  }
+  
+})
+
 app.get('/info', (request, response) => {
   response.send(`Phonebook has info for ${phoneBook.length} people <br/> <br/> ${Date()}`)
+})
+
+app.delete('/api/persons/delete/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phoneBook = phoneBook.filter(c => c.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const newId = Math.floor(Math.random() * 10000000) + Math.max(...phoneBook.map(i => i.id))
+  return newId
+}
+
+app.post('/api/persons', express.json(),(request, response) => {
+  const body = request.body
+  if (body.name && body.phoneNumber) {
+    if (phoneBook.find(n => n.name == body.name)) {
+      return response.status(409).json({error: "Name must be unique"})
+    }
+    const contact = {
+      id: generateId(),
+      name: body.name,
+      number: Number(body.phoneNumber)
+    }
+    phoneBook = phoneBook.concat(contact)
+    response.json(contact)
+  } else {
+    return response.status(400).json({
+      error:  'Persons name or number is missing'
+    })
+  }
 })
 
 const PORT = 3001
